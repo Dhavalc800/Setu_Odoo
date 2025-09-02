71a685889cb259e5c058818e78b0ffb64345682e
Dhaval Chavda <dhavalc800@gmail.com>
2025-09-02 10:38:07 +0530

    [ADD] Add access module.

---
+++ insta_biz_user_access/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'User Default Access',
+    'version': '16.0.1.0.0',
+    'summary': 'Set default access rights for new users',
+    'author': 'Insta Biz Web',
+    'depends': ['base', 'sales_team', 'account', 'hr', 'project'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
